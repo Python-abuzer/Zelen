90c3cb85eace4a348612cab0d5d2785fa17cdec7
Kompot <abramovegor526@gmail.com>
2024-09-29 23:27:49 +0300

    -

---
+++ src/Card.py
@@ -0,0 +1,27 @@
+
+class Card:
+    vegetables = ["tomato","carrot","corn","eggplant"]
+    price = list(range(6))
+
+    def __init__(self,vegs:str,pr:int):
+        if vegs not in Card.vegetables:
+            raise ValueError
+        if pr not in Card.price:
+            raise ValueError
+        self.vegs = vegs
+        self.pr = pr
+
+    def __repr__(self):
+        return f'{self.vegs}{self.pr}'
+    
+    def __eq__(self, other):
+        if isinstance(other, str):
+            other = Card.load(other)
+        return self.vegs == other.vegs and self.pr == other.pr
+
+    def save(self):
+        return repr(self)    
+    
+    @staticmethod
+    def load(text: str):
+        return Card(vegs=text[0], pr=int(text[1]))
