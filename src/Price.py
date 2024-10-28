afc89130cc5e8bfe1fa6bccd4e39db2e964bc87b
Kompot <abramovegor526@gmail.com>
2024-10-28 12:35:40 +0300

    Полностью исправил класс "Cards" + добавил тесты

--- src/Price.py
+++ src/Price.py
@@ -1,29 +1,71 @@
 "Цена"
+import pytest
+from typing import Self
+from src.сard import Card
+
 class Price:
-    def __init__(self,t=0,b=0,c=0,e=0):
-        if 0 <= t <= 5:
-            if 0 <= b <= 5:
-                if 0 <= c <= 5:
-                    if 0 <= e <= 5:
-                        self.t = t
-                        self.b = b
-                        self.c = c
-                        self.e = e
-        else:
+    VEGETABLES = Card.VEGETABLES
+    MAX_PRICE = 5
+    
+    def __init__(self, **kwargs):
+        for v in self.VEGETABLES:
+            setattr(self, v, kwargs.get(v))
+        self.d = kwargs.copy()
+        self.validate_vegetables(kwargs.keys())
+        self.validate_values()
+
+    def validate_vegetables(self, symbols):
+        if symbols not in self.VEGETABLES:
+            raise ValueError
+        
+    def validate_values(self, **kwargs):
+        if kwargs.values() not in range(6):
             raise ValueError
         
     def __repr__(self):
-        return f'{self.t}{self.b}{self.c}{self.e}'
+        return ''.join(list(f'{v}:{getattr(self, v)} ' for v in self.VEGETABLES))
     
-    def __eq__(self,other):
-        if isinstance(other, str):
-            return self.t == other.t and self.b == other.b and self.c == other.c and self.e == other.e
-        
-    def save(self):
-        return repr(self)
-    
-    @staticmethod
-    def add(price_str:str,card:str):
-        """Присваивание цены к карте"""
-        price = Price.load(price_str)
-        return f'{card}{price.save()}'
+    def __eq__(self, other):
+       return self.t == other.t and self.b == other.b and self.c == other.c and self.e == other.e and self.cr == other.cr
+
+    def add(self, other: str | dict | Self | Card):
+        if isinstance(other, Price):
+            other = other.d
+        elif isinstance(other, dict):
+            other = list(other.keys())
+        elif isinstance(other, Card):
+            other = list(str(other))
+        elif isinstance(other, str):
+            other = list(other)
+        for v in other:
+            if hasattr(self, v):
+                setattr(self, v, (1 + getattr(self, v)) % (Price.MAX_PRICE + 1))
+                self.d[v] = getattr(self, v)  
+            else:
+                print(f"Некорректный овощ: {v}") 
+
+
+
+def test_init():
+    p = Price(t=3, b=1, co=2, e=0, cr=4)
+    assert p.t == 3
+    assert p.b == 1
+    assert p.co == 2
+    assert p.e == 0
+    assert p.cr != 3
+
+def test_valid():
+    with pytest.raises(ValueError):
+        n = Price(t=52)
+
+def test_save():
+    p = Price(t=3, b=1, co=2, e=0, cr=4)
+    assert repr(p) == 't:3 b:1 co:2 e:0 cr:4'
+
+def test_add():
+    v = Price(t=0, b=1, co=2, e=3, cr=4)
+    v.add('tbb')
+    assert v.t == 0
+    assert v.b == 1
+    assert v.co == 3
+    assert v.e == 5
--- src/сard.py
+++ src/сard.py
@@ -1,27 +1,87 @@
 "Карты"
+import pytest
 class Card:
-    vegetables = ["t","b","c","e"] #tomato,broccoli,corn,eggplant
-    quantity = list(range(1,4))
+    VEGETABLES = ["t","b","co","e","cr"] #tomato,broccoli,corn,eggplant,carrot
+    QUANTITY = 3
 
-    def __init__(self,vegs:str,quant:int):
-        if vegs not in Card.vegetables:
+    def __init__(self,**kwargs):
+        if not kwargs or sum(kwargs.values()) != self.QUANTITY:
             raise ValueError
-        if quant not in Card.quantity:
-            raise ValueError
-        self.vegs = vegs
-        self.quant = quant
-
+        for veg in self.VEGETABLES:
+            setattr(self, veg, kwargs[veg])
     def __repr__(self):
-        return f'{self.vegs}{self.quant}'
+        return ''.join([veg * getattr(self, veg) for veg in self.VEGETABLES])
     
     def __eq__(self, other):
-        if isinstance(other, str):
-            other = Card.load(other)
-        return self.vegs == other.vegs and self.quant == other.quant
+       return self.t == other.t and self.b == other.b and self.c == other.c and self.e == other.e and self.cr == other.cr
 
     def save(self):
-        return repr(self)    
+        return repr(self)   
     
     @staticmethod
     def load(text: str):
-        return Card(vegs=text[0], quant=int(text[1]))
+        return Card(text.count('t'), text.count('b'), text.count('co'), text.count('e'), text.count('cr'))  
+    
+    @staticmethod
+    def all_cards(VEGETABLES: list[str] | None):
+        if VEGETABLES is None:
+            VEGETABLES = Card.VEGETABLES
+        cards = [Card.load(veg1 * 2 + veg2 * 1) for veg1 in VEGETABLES for veg2 in VEGETABLES]
+        cards = list(set(cards))
+        r_card = [Card.load(veg * 3) for veg in VEGETABLES]
+        for card in r_card:
+            if card in cards:
+                cards.remove(card)
+        return cards
+    
+def test_init():
+    c = Card(t=2, co=1)
+    assert c.t == 2
+    assert c.c0 == 1
+    assert c.b == 0
+    assert c.e == 0
+    assert c.cr == 0
+
+
+def test_save():
+    c = Card(t=3)
+    assert repr(c) == 'ttt'
+    assert c.save() == 'ttt'
+    c = Card(c0=2, cr=1)
+    assert repr(c) == 'cococr'
+    assert c.save() == 'cococr'
+
+
+def test_load():
+    z = 'ttb'
+    x = 'ttt'
+    c = 'eecr'
+    d = Card.load(x)
+    y = Card.load(z)
+    q = Card.load(c)
+    assert y == Card(t=2, b=1)
+    assert d == Card(t=3)
+    assert q == Card(e=2, cr=1)
+
+
+def test_eq():
+    c1 = Card(t=3)
+    c2 = Card(t=3)
+    c3 = Card(b=1, e=2)
+    c4 = Card(co=2, cr=1)
+    assert c1 == c2
+    assert c1 != c3
+    assert c2 != c3
+    assert c1 != c4
+
+def test_all_cards():
+    cards = Card.all_cards(['t', 'b'])
+    expected_cards = [
+        Card.load('ttb'),
+        Card.load('tbb'),
+        Card.load('ttt'),
+        Card.load('ttb'),
+        Card.load('tbb'),
+        Card.load('bbb')
+    ]
+    assert cards == expected_cards
